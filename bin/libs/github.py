57bf754f9e3979fc235534975f710344c8415272
Hugo Chilemme <hugo.chilemme@gmail.com>
2023-04-12 20:51:04 +0000

    v0.2: Creation of a new interface

--- bin/libs/github.py
+++ bin/libs/github.py
@@ -10,8 +10,8 @@ def find_commits_by_users_and_searchterm(users=[], searchterm=""):
     userstring = "+user%3A".join(users)
 
     # URL formation
-    url = f"https://github.com/search?o=desc&q={searchterm}+user%3A{userstring}&s=updated&type=Repositories"
-
+    url = f"https://github.com/search?o=desc&q=+user%3A{userstring}&s=updated&type=Repositories"
+    
     # Found commits will be stored here
     commits = []
 
@@ -50,6 +50,7 @@ def find_commits_by_users_and_searchterm(users=[], searchterm=""):
                 commits.append({
                     'USER_ID': userId, 
                     'ID': id, 
+                    'TYPE': "Commits",
                     'REPOSITORY': repository,
                     'DATETIME': datetime
                 })
@@ -57,6 +58,67 @@ def find_commits_by_users_and_searchterm(users=[], searchterm=""):
     # Returns the list of commits
     return commits
 
+
+def find_pullrequests_by_users_and_searchterm(users=[], searchterm=""):
+
+    # Creating a string with +user%3A as delimiter
+    userstring = "+user%3A".join(users)
+
+    # URL formation
+    url = f"https://github.com/search?o=desc&q=+user%3A{userstring}&s=updated&type=Issues"
+    
+    # Found commits will be stored here
+    commits = []
+
+    # Sending the request
+    r = requests.get(url)
+
+    # Status check : 200 = SUCCESS
+    if r.status_code == 200:
+
+        # Conversion of string to html parse
+        soup = BeautifulSoup(r.content, 'html.parser')
+
+        # We retrieve all items that are supposed to be commits
+        repoListItems = soup.find_all(class_='issue-list-item')
+
+        # We reverse the list to start from the oldest commit to the most recent
+        repoListItems.reverse()
+
+        # Analysis of each commit
+        for item in repoListItems:
+
+            # We verify that the element we have retrieved is indeed a commit
+            href = item.find(class_='text-bold')['href']
+            title = item.find(class_='markdown-title').find('a')['title']
+            
+            if href is not None:
+                # Get Github ID and Repository by url
+                repository = href.split('/')[2]
+                userId = None
+                textBolds = item.find_all(class_='text-bold')
+                
+                for textBold in textBolds:
+                    if textBold.get('title') is not None:
+                        userId = textBold['title']
+                
+            #     # Commit date retrieval
+                datetime = item.find('relative-time')['datetime']
+
+                # Creating a fake but unique ID
+                id = hashlib.md5(f"{userId}-{datetime}".encode('utf-8')).hexdigest()
+
+                commits.append({
+                    'USER_ID': userId, 
+                    'ID': id, 
+                    'TYPE': 'Issues',
+                    'TITLE': title,
+                    'REPOSITORY': repository,
+                    'DATETIME': datetime
+                })
+              
+    # Returns the list of commits
+    return commits
 
 def find_last_commit_by_repository(userId, repository):
     
@@ -92,8 +154,8 @@ def save_commit_to_database(commit):
     
     # Structuring of the request
     query = """
-        INSERT INTO commits(ID, USER_ID, REPOSITORY, TITLE, DATETIME, XP)
-        VALUES(%(id)s, %(user_id)s, %(repository)s, %(title)s, %(datetime)s, %(xp)s)
+        INSERT INTO commits(ID, USER_ID, REPOSITORY, TITLE, DATETIME, XP, XP_BEFORE)
+        VALUES(%(id)s, %(user_id)s, %(repository)s, %(title)s, %(datetime)s, %(xp)s, %(xp_before)s)
     """
     
     # Data to insert
@@ -103,7 +165,8 @@ def save_commit_to_database(commit):
         'repository': commit['REPOSITORY'],
         'title': commit['TITLE'],
         'datetime': commit['DATETIME'],
-        'xp': commit['XP']
+        'xp': commit['XP'],
+        'xp_before': commit['XP_BEFORE']
     }
 
     # Query Execution
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python3
 from threading import Thread
 from bin.modules.setTimeout import setTimeout
-from bin.libs.github import find_commits_by_users_and_searchterm, find_last_commit_by_repository, save_commit_to_database
-from bin.libs.database import get_table
+from bin.libs.github import find_commits_by_users_and_searchterm,find_pullrequests_by_users_and_searchterm,  find_last_commit_by_repository, save_commit_to_database
+from bin.libs.database import get_table, execute
 from bin.labs.analyzer import get_xp_from_title
 from flask import Flask, request
 from datetime import datetime
+
+import requests
+import time
 
 app = Flask(__name__)
 
@@ -14,15 +17,28 @@ db_commits = get_table('commits', "ORDER BY ORDER_ID DESC")
 oldest_commits = {commit['ID']: commit for commit in db_commits}
 
 # Create a list of Github usernames from the 'users' table
-users = get_table('users')
-github_list = [user['Github'] for user in users if user.get('Github')]
+users_table = get_table('users WHERE inactive = 0 AND Github IS NOT NULL')
+github_list = [user['Github'] for user in users_table if user.get('Github')]
+users = {user['Github']: user for user in users_table}
+
 need_display = []
+for commit in oldest_commits:
+    need_display.append(commit)
+
+typemode = "Repositories"
 
 def process():
+    return 
+    global typemode
     """ main function """
     
-    # Retrieving the last 20 newsfeed commits with the research topic "holberton-"
-    commits = find_commits_by_users_and_searchterm(github_list, "holberton-")
+    commits = []
+    if typemode == "Repositories":
+        commits = find_commits_by_users_and_searchterm(github_list, "holberton-")
+        typemode = "Issues"
+    else:
+        commits = find_pullrequests_by_users_and_searchterm(github_list, "holberton-")
+        typemode = "Repositories"
     
     for commit in commits:
         
@@ -36,29 +52,33 @@ def process():
             
                 
             # Information about the commit retrieve
-            CommitInfo = find_last_commit_by_repository(userId, repository)
+            if commit.get('TITLE') is None:
+                
+                CommitInfo = find_last_commit_by_repository(userId, repository)
             
-            # Title recovery
-            title = CommitInfo['title']
-            
+                commit['TITLE'] = CommitInfo['title']
+                
+                if CommitInfo.get('author') is not None :
+                
+                    author = CommitInfo.get('author')
+                    
+                    # If the author is a Toulouse holbie
+                    if author in github_list:
+                        
+                        # We change the author of the commit
+                        commit['USER_ID'] = author
+                
             # we score the title of the commit
-            experience = get_xp_from_title(title)
+            experience = get_xp_from_title(commit.get('TITLE'))
             
             # Add information in the commit
             commit['USER_ID'] = userId
-            commit['TITLE'] = title
             commit['XP'] = experience
             
+            commit['XP_BEFORE'] = users[userId]['XP']
+            commit['DISPLAYED'] = 0
+            users[userId]['XP'] += experience
             # The system may have detected that the author is not the creator of the repo
-            if CommitInfo.get('author') is not None :
-                
-                author = CommitInfo.get('author')
-                
-                # If the author is a Toulouse holbie
-                if author in github_list:
-                    
-                    # We change the author of the commit
-                    commit['USER_ID'] = author
                 
             
             # We have it on the "blacklist" of the next recoveries
@@ -69,6 +89,7 @@ def process():
             
             # Database Saving
             save_commit_to_database(commit)
+            execute("UPDATE users SET XP = {} WHERE Github = '{}'".format(users[userId]['XP'], userId))
             
             # Announce in console
             print(f"New commit of {userId} ({experience}): {commit.get('TITLE')}")
@@ -77,44 +98,155 @@ def process():
     # We restart the process indefinitely every 15 seconds
     setTimeout(process, 15000)
     
+
+def get_ranks():
+    def sort_by_xp(user):
+        return user[1]["XP"]
+    
+    def is_eligible(user):
+        if user[1]['XP'] != 0:
+            return True
+        return False
+    
+    active_users = dict(filter(is_eligible, users.items()))
+    sorted_users = dict(sorted(active_users.items(), key=sort_by_xp, reverse=True))
+    return sorted_users
+
+def get_rank(user):
+    ranks = get_ranks()
+    position = 0
+    
+    for rank in ranks.values():
+        position += 1
+        if rank.get('Github') == user.get('Github'):
+            return position
+ 
+ 
 @app.route('/api/v2/commits')
 def home():
-    cf_connecting_ip = request.headers.get('Cf-Connecting-Ip')
+    
+    limit = request.args.get('limit')
+    limit_i = 0
+    
+    commits = need_display[:10]
+    callback = []
+    
+    for k, commit in oldest_commits.items():
         
-    commit = {}
-    if len(need_display) > 0:
-        commit = need_display[0]
-        target = None
-        for user in users:
-            if user.get('Github') == commit.get('USER_ID'):
-                target = user
-                break
-        
-        commit['USER'] = {
-            "Pseudo": target['Pseudo'],
-            "Avatar": target['Avatar'],
-            "Session": target['Session'],
-        }
-        
-        if cf_connecting_ip == "80.125.5.170":
-            need_display.pop(0)
-            print('deleted')
+        if commit.get('DISPLAYED') == 0:
             
-    return commit
+            if limit is None or limit_i < int(limit):
+                user = users[commit.get('USER_ID')]
+                
+                commit['USER'] = {
+                    'Session': user.get('Session'),
+                    'Pseudo': user.get('Pseudo'),
+                    'Avatar': user.get('Avatar'),
+                }
+                                    
+                rank = get_rank(user)
+                commit['USER']['Rank'] = rank
+                callback.append(commit)
+                limit_i += 1
+            
+    return callback
 
+
+@app.route('/api/v2/commits/<commit_id>')
+def commits_by_id(commit_id):
+    if oldest_commits.get(commit_id) is not None:
+        
+        commit = oldest_commits.get(commit_id)
+        user = users[commit.get('USER_ID')]
+            
+        commit['USER'] = {
+            'Session': user.get('Session'),
+            'Pseudo': user.get('Pseudo'),
+            'Avatar': user.get('Avatar'),
+        }
+            
+        return commit
+    
+    
+@app.route('/api/v2/commits/<commit_id>/setDisplayed')
+def commits_setDisplayed(commit_id):
+    cf_connecting_ip = request.headers.get('Cf-Connecting-Ip')
+    
+    if oldest_commits.get(commit_id) is not None: 
+    # and cf_connecting_ip == "80.125.5.170":
+        oldest_commits[commit_id]['DISPLAYED'] = 1
+        execute('UPDATE commits SET DISPLAYED = 1 WHERE ID = %(id)s', { 'id': commit_id })
+            
+    return { "ok": True}
+
+
+@app.route('/api/v2/scoreboard')
+def scoreboard():
+    json = []
+    
+    def sort_by_xp(user):
+        return user[1]["XP"]
+    
+    for user in users.values():
+        if user.get('Github') and user.get('XP') != 0:
+            json.append({
+                "Rank": get_rank(user),
+                "Experience": user.get('XP'),
+                "Pseudo": user.get('Pseudo'),
+                "Avatar": user.get('Avatar'),
+                "Session": user.get('Session'),
+                "Login": user.get('Github'),
+            })
+    json = sorted(json, key=lambda u: u['Rank'], reverse=True)
+    return json
 
 @app.route('/api/v2/version')
 def version():
     return {
         "manifest": True,
-        "version": "v0.0.18-beta",
+        "version": "v0.2.87-beta",
     }
+
 
 @app.route('/api/v2/address')
 def address():
     cf_connecting_ip = request.headers.get('Cf-Connecting-Ip')
     return cf_connecting_ip
 
+
+# Initialiser le dictionnaire pour stocker temporairement la température
+temperature_cache = {
+    "temperature": None,
+    "timestamp": 0
+}
+
+@app.route('/api/v2/weather')
+def weather():
+    # latitude et longitude pour Toulouse, France
+    latitude = '43.6045'
+    longitude = '1.4440'
+    # URL pour l'API de météo ouverte
+    url = f'https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&current_weather=true'
+
+    # Vérifier si la température est en cache et si elle a été mise à jour il y a moins d'une minute
+    current_time = time.time()
+    if temperature_cache['temperature'] is not None and (current_time - temperature_cache['timestamp']) < 60:
+        temperature = temperature_cache['temperature']
+    else:
+        # Envoyer la requête à l'API
+        response = requests.get(url)
+        # Extraire la température à partir de la réponse JSON
+        temperature = response.json()['current_weather']['temperature']
+        # Mettre à jour la température dans le cache
+        temperature_cache['temperature'] = temperature
+        temperature_cache['timestamp'] = current_time
+
+    # Retourner la température sous forme de dictionnaire JSON
+    return {
+        "temperature": temperature
+    }
+
+    
 
 def start_flask():
     app.run(port=9999)
